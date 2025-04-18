35fb5144125610d4253321b5ca0636c5df79a7a3
ph4nt0mer <rootphantomy@hotmail.com>
2025-04-18 18:10:04 +0800

    feat: initialize Vue 3 project with Vite, add main components and styles
    - Created main CSS file for global styles.
    - Added HelloWorld component with props and basic styling.
    - Implemented TheWelcome component with inline and block elements.
    - Added various icon components for community, documentation, ecosystem, support, and tooling.
    - Set up main application entry point with Pinia and Vue Router.
    - Created router configuration with home and about routes.
    - Developed counter store using Pinia for state management.
    - Created AboutView and HomeView components with responsive design.
    - Configured TypeScript settings for the application.
    - Set up Vite configuration for the project.

---
+++ baihu_vue/src/App.vue
@@ -0,0 +1,74 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div>
+      <div class="left">
+        <RouterLink to="/"><img id="logo" src="@/assets/logo.png" alt="Company Logo" /></RouterLink>
+      </div>
+      <div class="right">
+        <RouterLink to="/">主页</RouterLink>
+        <RouterLink to="/news">新闻</RouterLink>
+        <RouterLink to="/book">作品</RouterLink>
+        <RouterLink to="/contact">联系</RouterLink>
+        <RouterLink to="/about">关于</RouterLink>
+      </div>
+    </div>
+  </header>
+  <div class="content">
+    <RouterView />
+  </div>
+  <footer>
+    1111
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+$header: 20%;
+$foot: 10%;
+
+header {
+  width: 100%;
+  max-width: 100%;
+  height: $header;
+  padding-top: $header/5;
+}
+
+header div {
+  height: 100%;
+}
+
+.left {
+  display: inline;
+  height: 100%;
+}
+
+.right {
+  display: inline;
+  height: 100%;
+  float: right;
+  padding-right: 5%;
+}
+
+header div a {
+  color: red;
+  margin: 0 20px;
+  padding: 0 3em 0 0;
+
+}
+
+img {
+  display: block;
+}
+
+footer {
+  height: 10%;
+  text-align: center;
+}
+
+.content {
+  height: 70%;
+}
+</style>
---
+++ baihu_vue/src/components/TheWelcome.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>我是块级元素（独占一行）</div>
+  <span>我是内联元素（不换行）</span>
+  <p>我是内联元素（不换行）</p>
+</template>
+<style scoped>
+* {
+  text-align: center;
+}
+</style>
