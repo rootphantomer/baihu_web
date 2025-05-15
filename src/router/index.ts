f668fa317194f490e57790a8e47d62bc7eb3c797
ph4nt0mer <rootphantomy@hotmail.com>
2025-05-15 10:27:14 +0800

    refactor(router): 注释掉未使用的视图导入以清理代码

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,9 +1,9 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
-import BookView from '@/views/BookView.vue'
-import NewsView from '@/views/NewsView.vue'
-import AboutView from '@/views/AboutView.vue'
-import ContactView from '@/views/ContactView.vue'
+// import BookView from '@/views/BookView.vue'
+// import NewsView from '@/views/NewsView.vue'
+// import AboutView from '@/views/AboutView.vue'
+// import ContactView from '@/views/ContactView.vue'
 
 const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
@@ -13,34 +13,34 @@ const router = createRouter({
       name: 'home',
       component: HomeView,
     },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: AboutView,
-    },
-    {
-      path: '/news',
-      name: 'news',
-      component: NewsView,
-    },
-    {
-      path: '/book',
-      name: 'book',
-      component: BookView,
-    },
-    {
-      path: '/contact',
-      name: 'contact',
-      component: ContactView,
-    },
     // {
-    //   path: '/article/:id',
-    //   name: 'article:id',
+    //   path: '/about',
+    //   name: 'about',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (About.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: AboutView,
+    // },
+    // {
+    //   path: '/news',
+    //   name: 'news',
+    //   component: NewsView,
+    // },
+    // {
+    //   path: '/book',
+    //   name: 'book',
+    //   component: BookView,
+    // },
+    // {
+    //   path: '/contact',
+    //   name: 'contact',
     //   component: ContactView,
     // },
+    // // {
+    // //   path: '/article/:id',
+    // //   name: 'article:id',
+    // //   component: ContactView,
+    // // },
   ],
 })
 
