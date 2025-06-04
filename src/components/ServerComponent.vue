14979df23642576d0403e94907f1d0de40a814cb
ph4nt0mer <rootphantomy@hotmail.com>
2025-06-04 17:25:20 +0800

    feat(组件): 优化服务组件描述，添加段落标签以改善文本结构和可读性

--- src/components/ServerComponent.vue
+++ src/components/ServerComponent.vue
@@ -8,9 +8,12 @@
           <span>动画制作服务</span>
         </div>
         <div class="server-desc">
-          凭借核心团队在业界深耕多年的技术力专注优质2D动画制作，并拥有制作高质量游戏PV与产品推广动画的丰富经验。<br />
-          从创意到成片，以精湛技艺打造极致内容，为您的IP与品牌注入超凡视觉魅力。
+          <p> 凭借核心团队在业界深耕多年的技术力专注优质2D动画制作，并拥有制作高质量游戏PV与产品推广动画的丰富经验。</p>
+          <p>从创意到成片，以精湛技艺打造极致内容，为您的IP与品牌注入超凡视觉魅力。</p>
         </div>
+        <br />
+        <br />
+        <br />
         <br />
         <div class="server-col">
           <div class="server-title">
@@ -18,7 +21,7 @@
             <span>专业前期设计</span>
           </div>
           <div class="server-desc">
-            创意赋能，设计领航。团队提供剧本分镜、角色场景设计、美术风格定制等全流程前期服务，用匠心打造独特视觉叙事，为您的项目奠定卓越基石。
+            <p>创意赋能，设计领航。团队提供剧本分镜、角色场景设计、美术风格定制等全流程前期服务，用匠心打造独特视觉叙事，为您的项目奠定卓越基石。</p>
           </div>
         </div>
       </div>
@@ -29,18 +32,19 @@
           <span>覆盖范围</span>
         </div>
         <div class="server-desc">
-          动画电影（2D/3D 长篇或短片）<br />
-          剧集动画（TV动画、网络番剧）<br />
-          广告PV（品牌宣传片、产品动画）<br /><br />
-          角色动画（Idle、Attack、Run等动作库）<br />
-          过场动画（Cinematic Cutscenes）
+          <p>动画电影（2D/3D 长篇或短片</p>
+          <p>剧集动画（TV动画、网络番剧</p>
+          <p>广告PV（品牌宣传片、产品动画</p>
+          <br>
+          <p>角色动画（Idle、Attack、Run等动作库</p>
+          <p>过场动画（Cinematic Cutscenes）</p>
+          <br />
         </div>
         <div class="server-col">
-          <br />
           <div class="server-desc">
-            剧本与分镜（Storyboard & Scriptwriting）<br />
-            概念设计（角色/场景/道具原画）<br />
-            美术风格定调（Color Script & Mood Board）
+            <p>剧本与分镜（Storyboard & Scriptwriting）</p>
+            <p>概念设计（角色/场景/道具原画</p>
+            <p>美术风格定调（Color Script & Mood Board）</p>
           </div>
         </div>
       </div>
@@ -67,6 +71,10 @@
   min-width: 34rem;
   max-width: 60rem;
   text-align: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 }
 
 .server-title {
