5c9751f3d8764e3e280c6744e50f5094242d9a48
ph4nt0mer <rootphantomy@hotmail.com>
2025-06-06 18:33:52 +0800

    refactor(PhotoComponent): 更新照片列表并替换旧图片
    移除不再使用的旧图片文件，并添加新的图片到照片列表

--- src/components/PhotoComponent.vue
+++ src/components/PhotoComponent.vue
@@ -26,13 +26,13 @@ const photos = [
   '/img_photo/025cd9bde8dce16bcc7180026054286.jpg',
   '/img_photo/65f60a9002870c118f789852c219e85.jpg',
   '/img_photo/650b088453e5b07cf41430f3361f160.jpg',
-  '/img_photo/680b97d05ea82731542cfee6694bfd2.jpg',
+  '/img_photo/WechatIMG319.jpg',
   '/img_photo/b0a61f6c82b4b0fbc62a8c391149e3f.jpg',
   '/img_photo/beb5568d28838c6648cf8de7d36e252.jpg',
   '/img_photo/d4bd9110d4013c05a2354c6a52633e0.jpg',
-  '/img_photo/WechatIMG445.jpg',
-  '/img_photo/WechatIMG304.jpg',
-  '/img_photo/WechatIMG305.jpg',
+  '/img_photo/WechatIMG318.jpg',
+  '/img_photo/WechatIMG320.jpg',
+  '/img_photo/WechatIMG321.jpg',
 ]
 const previewImg = ref<string | null>(null)
 
