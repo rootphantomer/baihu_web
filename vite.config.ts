91989d855fdff50d173a82ddbfa3375c58795acc
ph4nt0mer <rootphantomy@hotmail.com>
2025-06-18 11:27:49 +0800

    build: 移除未使用的插件并添加对svg文件的支持
    移除被注释掉的ElementPlus相关插件配置
    在assetsInclude中添加对svg文件的支持

--- vite.config.ts
+++ vite.config.ts
@@ -4,22 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// import AutoImport from 'unplugin-auto-import/vite'
-// import Components from 'unplugin-vue-components/vite'
-// import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
-
-// https://vite.dev/config/
 export default defineConfig({
   base: './', // 确保正确的路径到你的 HTML 文件
   plugins: [
     vue(),
     vueDevTools(),
-    // AutoImport({
-    //   resolvers: [ElementPlusResolver()],
-    // }),
-    // Components({
-    //   resolvers: [ElementPlusResolver()],
-    // }),
   ],
   resolve: {
     alias: {
@@ -29,7 +18,7 @@ export default defineConfig({
   esbuild: {
     drop: ['console', 'debugger'],
   },
-  assetsInclude: ['**/*.png'], // 确保包含 PNG 图片
+  assetsInclude: ['**/*.png', '**/*.svg'], // 确保包含 PNG 图片
   build: {
     target: ['chrome90', 'edge90', 'safari13'],
     outDir: 'dist',
